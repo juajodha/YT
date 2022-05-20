6c71a4fdd95de3c955afa836d493191e539a48fe
juajodha <89195607+juajodha@users.noreply.github.com>
2022-05-20 21:35:47 +0700

    Update gen.js

--- gen.js
+++ gen.js
@@ -95,7 +95,7 @@ function nameGen(type){
 	  tag9=Math.floor(Math.random()*tanda9.length);
 	  tag10=Math.floor(Math.random()*tanda10.length);
 	  tag11=Math.floor(Math.random()*tanda11.length);
-          names=tanda23[tag23]+it1[rnd]+"\n"+tanda8[tag8]+tanda9[tag9]+tanda10[tag10]+tanda11[tag11]+"\n"+tanda22[tag22]+it2[rnd]+"\n"+tanda21[tag21];
+          names=tanda23[tag23]+"\n"+tanda8[tag8]+tanda9[tag9]+tanda10[tag10]+tanda11[tag11]+"\n"+tanda22[tag22]+"\n"+tanda21[tag21];
         }
         else{
           rnd=Math.floor(Math.random()*it2.length);
